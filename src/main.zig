ca11c14c0ea42de696c30b2442f48c38071321c8
Ali Aryani <lenesis@proton.me>
2025-10-26 04:16:22 +0330

    final phase and bug fixes

--- src/main.zig
+++ src/main.zig
@@ -8,25 +8,26 @@ const c = @cImport({
     @cInclude("stdio.h");
 });
 
-fn roll_dice() u8 {
-    return c.rand() % 6 + 1;
+fn roll_dice() c_int {
+    const roll:c_int = @intCast(@rem(c.rand(), 6) + 1);
+    return roll;
 }
 
-var player1:u8 = 0;
-var player2:u8 = 0;
+var player1:c_int = 0;
+var player2:c_int = 0;
 
 fn print_board() !void {
-    var board = [101]u8{};
+    var board:[101]u64 = undefined;
     for (1..101) |i| {
         board[i] = i;
     }
 
-    var alt = 0; // to specify alternates
-    var iterLR = 101; // iterator to print from left to right
-    var iterRL = 80; // iterator to print from right to left 
-    var val = 100;
+    var alt:c_int = 0; // to specify alternates
+    var iterLR:c_int = 101; // iterator to print from left to right
+    var iterRL:c_int = 80; // iterator to print from right to left 
+    var val:c_int = 100;
 
-    while (val) : (val-= 1) {
+    while (val!=0) : (val-= 1) {
         if (alt == 0) {
             iterLR -= 1;
             if (iterLR == player1) {
@@ -34,10 +35,10 @@ fn print_board() !void {
             } else if (iterLR == player2) {
                 try stdout.print("#P2    ", .{});
             } else {
-                try stdout.print("{d}    ", .{board[iterLR]});
+                try stdout.print("{d}    ", .{board[@intCast(iterLR)]});
             } try stdout.flush();
 
-            if (iterLR % 10 == 1) {
+            if (@mod(iterLR, 10) == 1) {
                 try stdout.print("\n\n", .{});
                 try stdout.flush();
                 alt = 1;
@@ -50,14 +51,14 @@ fn print_board() !void {
             } else if (iterRL == player2) {
                 try stdout.print("#P2    ", .{});
             } else {
-                try stdout.print("{d}    ", .{board[iterRL]});
+                try stdout.print("{d}    ", .{board[@intCast(iterRL)]});
             } try stdout.flush();
 
-            if (iterRL % 10 == 0) {
+            if (@mod(iterRL, 10) == 0) {
                 try stdout.print("\n\n", .{});
                 try stdout.flush();
                 alt = 0;
-                iterRL -= 30;
+                if(iterRL - 30 >= 0) iterRL -= 30;
             }
         }
         if (iterRL == 10)
@@ -68,10 +69,12 @@ fn print_board() !void {
 }
 
 // function to move player 
-fn move_player(current_player:u8,roll:u8) u8 {
-    const new_position = current_player + roll;
+fn move_player(current_player:c_int,roll:c_int) c_int {
+    const new_position:usize = @intCast(current_player + roll);
+    if (new_position > 100)
+        return current_player;
 
-    var snakes_and_ladders = [101]i16{};
+    var snakes_and_ladders:[101]c_int = undefined;
     for (0..101) |i| {
         snakes_and_ladders[i] = 0;
     }
@@ -79,14 +82,13 @@ fn move_player(current_player:u8,roll:u8) u8 {
     // positive values are ladders 
     // negative values are snakes 
     snakes_and_ladders[6] = 40;
-    snakes_and_ladders[24] = -10;
-    snakes_and_ladders[45] = -30;
+    snakes_and_ladders[23] = -10;
+    snakes_and_ladders[45] = -7;
     snakes_and_ladders[61] = -18;
-    snakes_and_ladders[58] = 22;
-    snakes_and_ladders[66] = 9;
-    snakes_and_ladders[98] = -20;
-
-    const new_square = new_position + snakes_and_ladders[new_position];
+    snakes_and_ladders[65] = -8;
+    snakes_and_ladders[98] = -10;
+    
+    const new_square:c_int = @intCast(@as(c_int,@intCast(new_position)) + snakes_and_ladders[@intCast(new_position)]);
     if (new_square > 100)
         return current_player;
     
@@ -94,19 +96,39 @@ fn move_player(current_player:u8,roll:u8) u8 {
 }
 
 pub fn main() !void {
-    c.srand(c.time(0));
-    var current_player = 1;
-    var won = false;
+    c.srand(@intCast(c.time(0)));
+    var current_player:c_int = 1;
+    var won:bool = false;
     
     try stdout.print("Snake And Ladder game\n", .{});
     while (!won) {
         try stdout.print("\nPlayer {d}, press Enter to roll the dice...", .{current_player});
         try stdout.flush();
-        c.getchar();
+        _ = c.getchar();
         const roll = roll_dice();
         try stdout.print("You rolled: {d}\n", .{roll});
         try stdout.flush();
 
-        
+        if (current_player == 1) {
+            player1 = move_player(player1, roll);
+            try stdout.print("Player 1 is now at square {d}\n\n", .{player1});
+            try print_board();
+            if (player1 == 100) {
+                try stdout.print("Player 1 wins!\n", .{});
+                try stdout.flush();
+                won = true;
+            }
+        } else {
+            player2 = move_player(player2, roll);
+            try stdout.print("Player 2 is now at square {d}\n\n", .{player2});
+            try print_board();
+            if (player2 == 100) {
+                try stdout.print("Player 2 wins!\n", .{});
+                try stdout.flush();
+                won = true;
+            }
+        }
+        // switch to other player 
+        current_player = if (current_player == 1) 2 else 1;
     }
 }
